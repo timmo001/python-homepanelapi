055d81b2004874c4fb40a70db5fca1968785eef6
Timmo <contact@timmo.xyz>
2019-10-10 20:52:17 +0100

    :hammer: Change version to 1.0.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
     LONG = fh.read()
 setuptools.setup(
     name="homepanelapi",
-    version="0.1.0",
+    version="1.0.0",
     author="Timmo",
     author_email="contact@timmo.xyz",
     description="",
